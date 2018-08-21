2bbc22ce3500fb716bdbd9f903302a7a064341f4
HyeongJu Yoo <zerofunc@naver.com>
2018-08-21 20:08:34 +0900

    날짜로 메모 정렬하기

---
+++ src/test/java/Test3.java
@@ -0,0 +1,141 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Test3 {
+    @Test
+    public void 패턴_매칭_테스트() {
+        final String memo = "2018-12-21 문이 잠겨있다.";
+
+        Pattern pattern = Pattern.compile("(19|2\\d)\\d{2}[-/년]((1[0-2]|0[1-9]|[1-9])[-/]([1-2]\\d|3[0-1]|[1-9]|0[1-9])|(1[0-2]|0[1-9]|[1-9])[월]([1-2]\\d|3[0-1]|[1-9]|0[1-9])일)");
+        Matcher a = pattern.matcher(memo);
+        List<String> formats = new ArrayList<String>();
+
+        formats.add("YYYY년M월D일");
+        formats.add("YYYY/M/D");
+        formats.add("YYYY-M-D");
+        Date memDate = null;
+        if(a.find()) {
+
+            memDate = formats.stream()
+                    .filter(str -> {
+                        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat(str);
+                        try {
+                            dateTimeFormatter.parse(a.group());
+                        } catch (ParseException e) {
+                            return false;
+                        }
+                        return true;
+                    })
+                    .map(str -> {
+                        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat(str);
+                        Date date = null;
+                        try {
+                            date = dateTimeFormatter.parse(a.group());
+                        } catch (ParseException e) {
+                        }
+                        return date;
+                    })
+                    .filter(date -> {
+                        return date != null;
+                    })
+                    .findAny().orElseGet(Date::new);
+
+        }
+        Assert.assertNotNull(memDate);
+        System.out.println(memDate.toString());
+    }
+
+    @Test
+    public void main() {
+        List<String> strings = new ArrayList<>();
+        strings.add("2018-12-21 문이 잠겨있다.");
+        strings.add("2016년4월1일 피자가 배달왔다");
+        strings.add("춤을 추고있다 - 2017/9/22 ");
+        Pattern pattern = Pattern.compile("(19|2\\d)\\d{2}[-/년]((1[0-2]|0[1-9]|[1-9])[-/]([1-2]\\d|3[0-1]|[1-9]|0[1-9])|(1[0-2]|0[1-9]|[1-9])[월]([1-2]\\d|3[0-1]|[1-9]|0[1-9])일)");
+        // 변환 포맷
+        List<String> formats = new ArrayList<String>();
+
+        formats.add("YYYY년M월D일");
+        formats.add("YYYY/M/D");
+        formats.add("YYYY-M-D");
+        strings.stream()
+                .map(s->{
+                    Memo memo = new Memo();
+                    // match이용해서 정규식으로 날짜 뽑기
+
+                        Matcher a = pattern.matcher(s);
+                        Date memoDate = null;
+                        if(a.find()) {
+                            memoDate = formats.stream()
+                                    .filter(str -> {
+                                        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat(str);
+                                        try {
+                                            dateTimeFormatter.parse(a.group());
+                                        } catch (ParseException e) {
+                                            return false;
+                                        }
+                                        return true;
+                                    })
+                                    .map(str -> {
+                                        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat(str);
+                                        Date date = null;
+                                        try {
+                                            date = dateTimeFormatter.parse(a.group());
+                                        } catch (ParseException e) {
+
+                                        }
+                                        return date;
+                                    })
+                                    .filter(date -> {
+                                        return date != null;
+                                    })
+                                    .findAny().orElseGet(Date::new);
+                        }
+
+                    memo.setDate(memoDate);
+                    memo.setMemo(s);
+                    return memo;
+                })
+                .sorted(Comparator.comparing(Memo::getDate))
+                .forEach((memo)-> {
+                    System.out.println(memo.getMemo());
+                });
+    }
+}
+
+
+class Memo {
+    private Date date;
+    private String memo;
+
+    public Date getDate(){
+        return date;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+}
